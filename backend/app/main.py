848045a081e73cf410065e5367bb4374c57fa29f
MJDesh <mugdha.deshpande2021@gmail.com>
2026-07-11 17:02:04 +0530

    Refactor FastAPI app structure and imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import bookings
-from routers import host
 
-from database import Base, engine
-from routers import listings, search
+from app.database import Base, engine
+from app.routers import bookings, host, listings, search
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Airbnb Clone API",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 app.include_router(listings.router)
@@ -20,11 +18,13 @@ app.include_router(host.router)
 
 @app.get("/")
 def root():
-    return {"message": "Airbnb Backend Running"}    
+    return {"message": "Airbnb Backend Running"}
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
